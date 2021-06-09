007aea5f25e19cc3a7740546369ac1b14733db48
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-06-08 21:43:29 -0500

    Building App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
 import './App.css';
 import Form from './components/Form';
 import * as yup from 'yup';
+import schema from './validation/formSchema';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialUser = []
+const initialDisabledState = true
 
 
 function App() {
+  const [user, setUser] = useState(initialUser)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabledState, setDisabledState] = useState(initialDisabledState)
+
+  const fetchUser = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(result => {
+        setUser(result.data)
+      })
+      .catch(error => {console.log(error)})
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
