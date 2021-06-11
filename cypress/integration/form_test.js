09770f26225b5182066053b24f5e908c7a3978c4
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-06-10 22:57:07 -0500

    Sanity Check / Name Field

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -2,17 +2,25 @@ describe('User Data Form', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000')
     })
+
+
+    it('sanity check', () => {
+        expect(2).to.equal(2)
+        expect(1 + 2).to.equal(3)
+        expect({}).to.eql({})
+        expect({}).not.to.equal({})
+    })
+
+    const nameField = () => cy.get('input[name="name"]')
+
+
+    it('typable field', () => {
+        nameField()
+            .should("have.value", "")
+            .type("Neo Anderson")
+            .should("have.value", "Neo Anderson")
+    })
+
+
+
 })
-
-it('sanity check', () => {
-    expect(2).to.equal(2)
-    expect(1 + 2).to.equal(3)
-    expect({}).to.eql({})
-    expect({}).not.to.equal({})
-})
-
-const nameInput = () => cy.get('input[name="name"]'
-
-
-
-)
